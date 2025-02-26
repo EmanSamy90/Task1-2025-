e5f804af0735793e2d1ea6560a83dfd721b99a5c
EmanSamy90 <es6723294@gmail.com>
2025-02-26 22:18:54 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 12, 11, 13, 5, 6 };
+            Sort(arr);
+            Console.WriteLine("\nSorted array:");
+            PrintArray(arr);
+        }
+        static void Sort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 1; i < n; i++)
+            {
+                int key = arr[i];
+                int j = i - 1;
+
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+        }
+        static void PrintArray(int[] arr)
+        {
+            foreach (int item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
